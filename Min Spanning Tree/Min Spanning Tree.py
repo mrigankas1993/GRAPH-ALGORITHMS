882b27c514dde9244b8d7cc0f495a3f13c9ffde6
Mriganka Saikia <95900533+mrigankas1993@users.noreply.github.com>
2022-04-24 13:20:58 +0530

    Update Min Spanning Tree.py

--- Min Spanning Tree/Min Spanning Tree.py
+++ Min Spanning Tree/Min Spanning Tree.py
@@ -1,5 +1,5 @@
 
-##minimum spanning tree kruskal's Algorithm Mrigank's implementation
+#kruskal's Algorithm to find minimum spanning tree Mrigank's implementation
 
 graph_edges = {'a' : 1, "b" : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7 , 'h' : 8, 'i' : 9 , 'j' : 10, 'k' : 11,
                'l' : 12, 'm' : 13, 'n' : 14, 'o' : 15, 'p' : 16, 'q' : 17, 'r' : 18}
